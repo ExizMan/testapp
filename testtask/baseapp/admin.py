a15b91388970e542811a85c90c85321dd1948071
ExizMan <yurasankin@yandex.ru>
2023-11-18 20:00:01 +0300

    models

--- testtask/baseapp/admin.py
+++ testtask/baseapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Employees,Professions
 
+admin.site.register(Employees)
+admin.site.register(Professions)
 # Register your models here.
--- testtask/baseapp/models.py
+++ testtask/baseapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
+
+class Employees(models.Model):
+    firstname = models.CharField(max_length=20, blank=False)
+    midname = models.CharField(max_length=20, blank=True, null=True)
+    lastname = models.CharField(max_length=20, blank=False)
+    profession = models.CharField(max_length=20, blank=False)
+    date_hired = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'employees'
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
+
+
+
+
+class Professions(models.Model):
+    tittle = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        db_table = 'professions'
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
+
+
 # Create your models here.
