792339ab1ba906976a08ed624bbb40abf6815cab
ExizMan <yurasankin@yandex.ru>
2023-11-20 00:08:36 +0300

    in processhg

--- testtask/baseapp/urls.py
+++ testtask/baseapp/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 app_name = 'baseapp'
 urlpatterns = [
+    path('',views.index, name="index"),
     path('employees',views.employees, name='employees'),
     path('professions',views.professions,name='professions'),
     path('employees/<int:emp_id>/',views.employee, name="employee"),
-    path('employees/<int:emp_id>/delete',views.del_employee(), name="del_employee"),
+    path('employees/<int:emp_id>/delete',views.del_employee, name="del_employee"),
     path('employees/new_employee/',views.new_employee, name="new_employee"),
 
 
